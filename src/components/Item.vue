22fd12bee45d0ae60362589e25b1b3c0d22d6fbb
David Nalin <david76545@gmail.com>
2023-09-26 15:43:19 +0200

    Implementazione item a schermo

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,20 +1,33 @@
 <template>
     <div>
-        <p class="d-inline-flex gap-1">
-            <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-                item
-            </a>
-        </p>
-        <div class="collapse" id="collapseExample">
-            <div class="card card-body">
-                content
+        <div class="row">
+            <p class="d-inline-flex gap-1">
+                <div class="card" data-bs-toggle="collapse" :href="'#'+id" role="button" aria-expanded="false" aria-controls="collapseExample">
+                    <div class="card-header">
+                        {{ title }}
+                    </div>
+                    <img :src=img class="ms-auto me-auto mt-3 mb-3" alt="img">
+                </div>
+            </p>
+            <div class="col-md-8">
+                <div class="collapse" :id=id>
+                    <p class="card-text">{{desc}}</p>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item" v-for="(item) in tags">{{ item }}</li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
-<script>
-    export default {
-        name: 'Item'
-    }
+<script setup>
+    const props = defineProps(['id', 'img', 'title', 'desc', 'tags'])
 </script>
+
+<style>
+    img {
+        max-width: 3rem;
+        height: auto;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,9 +8,9 @@
                         <li class="nav-item">
                             <a class="nav-link active" role="button">Visualizza</a>
                         </li>
-                        <li class="nav-item">
+                        <!-- <li class="nav-item">
                             <a class="nav-link" role="button">Crea nuovo</a>
-                        </li>
+                        </li> -->
                     </ul>
                 </div>
                 <div class="d-flex justify-content-end">
@@ -18,22 +18,36 @@
                 </div>
             </div>
         </li>
-        <!-- <div class="container mt-3" v-for="(item, index) in currentDocs">
-            {{ index }} - {{ item }}
-        </div> -->
-        <li ref="currentDocs"></li>
+        <div class="container mt-3">
+            <div class="d-inline" v-for="todo in list">
+                <Item :id="todo.id" :img="todo.img" :title="todo.title" :desc="todo.desc" :tags="todo.tags"/>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup>
-    import { onMounted } from 'vue'
+    import { ref, onMounted } from 'vue'
+
+    const list = ref([])
+
     onMounted(async () => {
+        let lList = []
         await getDocs(collection(db, 'data')).then((docs) => {
             docs.forEach((doc) => {
                 console.log(doc.id, " => ", doc.data());
-                //this.currentDocs[doc.id] = doc.data();
+                const data = doc.data()
+                const temp = {
+                    id: doc.id,
+                    img: data.img,
+                    title: data.title,
+                    desc: data.desc,
+                    tags: data.tags
+                }
+                lList.push(temp)
             });
         })
+        list.value = lList
     })
 </script>
 
@@ -43,12 +57,6 @@
     import SignOut from '@/components/SignOut.vue';
     import Item from '@/components/Item.vue';
     export default {
-        name: 'Home',
-        data() {
-            return {
-                currentDocs : []
-            }
-        },
         components: {
             SignOut,
             Item
